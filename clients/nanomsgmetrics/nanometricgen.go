d0637029e6ab6821ac52dd38e35fae0cb997f00d
Pavan Krishna Nimmagadda <pavankri@cisco.com>
2021-09-07 18:27:38 -0700

    added nanomsgs

--- clients/nanomsgmetrics/nanometricgen.go
+++ clients/nanomsgmetrics/nanometricgen.go
@@ -7,10 +7,8 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/translator/opencensus"
 	"os"
 
-	"go.opentelemetry.io/collector/model/otlp"
-	"google.golang.org/protobuf/types/known/timestamppb"
-
 	"go.nanomsg.org/mangos/v3/protocol/push"
+	"go.opentelemetry.io/collector/model/otlp"
 	// imported for side effects to register ipc/tcp transport
 	_ "go.nanomsg.org/mangos/v3/transport/ipc"
 	_ "go.nanomsg.org/mangos/v3/transport/tcp"
@@ -38,118 +36,12 @@ func (b builder) setName(name string) builder {
 	return b
 }
 
-// setLabels sets the labels for the metric
-func (b builder) setLabels(labels []string) builder {
-	labelKeys := make([]*metricspb.LabelKey, len(labels))
-	for i, l := range labels {
-		labelKeys[i] = &metricspb.LabelKey{
-			Key: l,
-		}
-	}
-	b.metric.MetricDescriptor.LabelKeys = labelKeys
-	return b
-}
-
-// addTimeseries adds new timeseries with the labelValuesVal and startTimestamp
-func (b builder) addTimeseries(startTimestampSeconds int64, labelValuesVal []string) builder {
-	labelValues := make([]*metricspb.LabelValue, len(labelValuesVal))
-	for i, v := range labelValuesVal {
-		labelValues[i] = &metricspb.LabelValue{
-			Value:    v,
-			HasValue: true,
-		}
-	}
-
-	var startTimestamp *timestamppb.Timestamp
-	if startTimestampSeconds != 0 {
-		startTimestamp = &timestamppb.Timestamp{Seconds: startTimestampSeconds}
-	}
-
-	timeseries := &metricspb.TimeSeries{
-		StartTimestamp: startTimestamp,
-		LabelValues:    labelValues,
-		Points:         nil,
-	}
-	b.metric.Timeseries = append(b.metric.Timeseries, timeseries)
-	return b
-}
-
 // setDataType sets the data type of this metric
 func (b builder) setDataType(dataType metricspb.MetricDescriptor_Type) builder {
 	b.metric.MetricDescriptor.Type = dataType
 	return b
 }
 
-// setUnit sets the unit of this metric
-func (b builder) setUnit(unit string) builder {
-	b.metric.MetricDescriptor.Unit = unit
-	return b
-}
-
-// addInt64Point adds a int64 point to the tidx-th timseries
-func (b builder) addInt64Point(tidx int, val int64, timestampVal int64) builder {
-	point := &metricspb.Point{
-		Timestamp: &timestamppb.Timestamp{
-			Seconds: timestampVal,
-			Nanos:   0,
-		},
-		Value: &metricspb.Point_Int64Value{
-			Int64Value: val,
-		},
-	}
-	points := b.metric.Timeseries[tidx].Points
-	b.metric.Timeseries[tidx].Points = append(points, point)
-	return b
-}
-
-// addDoublePoint adds a double point to the tidx-th timseries
-func (b builder) addDoublePoint(tidx int, val float64, timestampVal int64) builder {
-	point := &metricspb.Point{
-		Timestamp: &timestamppb.Timestamp{
-			Seconds: timestampVal,
-			Nanos:   0,
-		},
-		Value: &metricspb.Point_DoubleValue{
-			DoubleValue: val,
-		},
-	}
-	points := b.metric.Timeseries[tidx].Points
-	b.metric.Timeseries[tidx].Points = append(points, point)
-	return b
-}
-
-// addDistributionPoints adds a distribution point to the tidx-th timseries
-func (b builder) addDistributionPoints(tidx int, count int64, sum float64, bounds []float64, bucketsVal []int64) builder {
-	buckets := make([]*metricspb.DistributionValue_Bucket, len(bucketsVal))
-	for buIdx, bucket := range bucketsVal {
-		buckets[buIdx] = &metricspb.DistributionValue_Bucket{
-			Count: bucket,
-		}
-	}
-	point := &metricspb.Point{
-		Timestamp: &timestamppb.Timestamp{
-			Seconds: 1,
-			Nanos:   0,
-		},
-		Value: &metricspb.Point_DistributionValue{
-			DistributionValue: &metricspb.DistributionValue{
-				BucketOptions: &metricspb.DistributionValue_BucketOptions{
-					Type: &metricspb.DistributionValue_BucketOptions_Explicit_{
-						Explicit: &metricspb.DistributionValue_BucketOptions_Explicit{
-							Bounds: bounds,
-						},
-					},
-				},
-				Count:   count,
-				Sum:     sum,
-				Buckets: buckets,
-			},
-		},
-	}
-	points := b.metric.Timeseries[tidx].Points
-	b.metric.Timeseries[tidx].Points = append(points, point)
-	return b
-}
 
 // Build builds from the builder to the final metric
 func (b builder) build() *metricspb.Metric {
