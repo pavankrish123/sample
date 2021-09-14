ade132ef62c8b5f5e1ac2d0d1b3b14bfa73af45b
Pavan Krishna Nimmagadda <pavankri@cisco.com>
2021-09-14 16:39:14 -0700

    push

---
+++ clients/nanomsgmetrics/go.mod
@@ -0,0 +1,25 @@
+module nano
+
+go 1.17
+
+require (
+	github.com/census-instrumentation/opencensus-proto v0.3.0
+	github.com/open-telemetry/opentelemetry-collector-contrib/pkg/translator/opencensus v0.35.0
+	go.nanomsg.org/mangos/v3 v3.2.1
+	go.opentelemetry.io/collector/model v0.35.0
+)
+
+require (
+	github.com/Microsoft/go-winio v0.4.11 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/open-telemetry/opentelemetry-collector-contrib/internal/coreinternal v0.35.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20210604141403-392c879c8b08 // indirect
+	google.golang.org/grpc v1.40.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
--- clients/nanomsgmetrics/nanometricgen.go
+++ clients/nanomsgmetrics/nanometricgen.go
@@ -5,6 +5,7 @@ import (
 	metricspb "github.com/census-instrumentation/opencensus-proto/gen-go/metrics/v1"
 	resourcepb "github.com/census-instrumentation/opencensus-proto/gen-go/resource/v1"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/translator/opencensus"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"os"
 
 	"go.nanomsg.org/mangos/v3/protocol/push"
@@ -15,6 +16,7 @@ import (
 
 	"time"
 )
+
 
 type builder struct {
 	metric *metricspb.Metric
@@ -36,12 +38,118 @@ func (b builder) setName(name string) builder {
 	return b
 }
 
+// setLabels sets the labels for the metric
+func (b builder) setLabels(labels []string) builder {
+	labelKeys := make([]*metricspb.LabelKey, len(labels))
+	for i, l := range labels {
+		labelKeys[i] = &metricspb.LabelKey{
+			Key: l,
+		}
+	}
+	b.metric.MetricDescriptor.LabelKeys = labelKeys
+	return b
+}
+
+// addTimeseries adds new timeseries with the labelValuesVal and startTimestamp
+func (b builder) addTimeseries(startTimestampSeconds int64, labelValuesVal []string) builder {
+	labelValues := make([]*metricspb.LabelValue, len(labelValuesVal))
+	for i, v := range labelValuesVal {
+		labelValues[i] = &metricspb.LabelValue{
+			Value:    v,
+			HasValue: true,
+		}
+	}
+
+	var startTimestamp *timestamppb.Timestamp
+	if startTimestampSeconds != 0 {
+		startTimestamp = &timestamppb.Timestamp{Seconds: startTimestampSeconds}
+	}
+
+	timeseries := &metricspb.TimeSeries{
+		StartTimestamp: startTimestamp,
+		LabelValues:    labelValues,
+		Points:         nil,
+	}
+	b.metric.Timeseries = append(b.metric.Timeseries, timeseries)
+	return b
+}
+
 // setDataType sets the data type of this metric
 func (b builder) setDataType(dataType metricspb.MetricDescriptor_Type) builder {
 	b.metric.MetricDescriptor.Type = dataType
 	return b
 }
 
+// setUnit sets the unit of this metric
+func (b builder) setUnit(unit string) builder {
+	b.metric.MetricDescriptor.Unit = unit
+	return b
+}
+
+// addInt64Point adds a int64 point to the tidx-th timseries
+func (b builder) addInt64Point(tidx int, val int64, timestampVal int64) builder {
+	point := &metricspb.Point{
+		Timestamp: &timestamppb.Timestamp{
+			Seconds: timestampVal,
+			Nanos:   0,
+		},
+		Value: &metricspb.Point_Int64Value{
+			Int64Value: val,
+		},
+	}
+	points := b.metric.Timeseries[tidx].Points
+	b.metric.Timeseries[tidx].Points = append(points, point)
+	return b
+}
+
+// addDoublePoint adds a double point to the tidx-th timseries
+func (b builder) addDoublePoint(tidx int, val float64, timestampVal int64) builder {
+	point := &metricspb.Point{
+		Timestamp: &timestamppb.Timestamp{
+			Seconds: timestampVal,
+			Nanos:   0,
+		},
+		Value: &metricspb.Point_DoubleValue{
+			DoubleValue: val,
+		},
+	}
+	points := b.metric.Timeseries[tidx].Points
+	b.metric.Timeseries[tidx].Points = append(points, point)
+	return b
+}
+
+// addDistributionPoints adds a distribution point to the tidx-th timseries
+func (b builder) addDistributionPoints(tidx int, count int64, sum float64, bounds []float64, bucketsVal []int64) builder {
+	buckets := make([]*metricspb.DistributionValue_Bucket, len(bucketsVal))
+	for buIdx, bucket := range bucketsVal {
+		buckets[buIdx] = &metricspb.DistributionValue_Bucket{
+			Count: bucket,
+		}
+	}
+	point := &metricspb.Point{
+		Timestamp: &timestamppb.Timestamp{
+			Seconds: 1,
+			Nanos:   0,
+		},
+		Value: &metricspb.Point_DistributionValue{
+			DistributionValue: &metricspb.DistributionValue{
+				BucketOptions: &metricspb.DistributionValue_BucketOptions{
+					Type: &metricspb.DistributionValue_BucketOptions_Explicit_{
+						Explicit: &metricspb.DistributionValue_BucketOptions_Explicit{
+							Bounds: bounds,
+						},
+					},
+				},
+				Count:   count,
+				Sum:     sum,
+				Buckets: buckets,
+			},
+		},
+	}
+	points := b.metric.Timeseries[tidx].Points
+	b.metric.Timeseries[tidx].Points = append(points, point)
+	return b
+}
 
 // Build builds from the builder to the final metric
 func (b builder) build() *metricspb.Metric {
@@ -49,9 +157,8 @@ func (b builder) build() *metricspb.Metric {
 }
 
 const (
-	metricsAddr = "tcp://0.0.0.0:45001"
+       metricsAddr = "tcp://0.0.0.0:45001"
 )
-
 
 func main(){
 	mSock, err := push.NewSocket()
@@ -64,7 +171,7 @@ func main(){
 		panic(err)
 	}
 
-	for i := 0; i < 50; i ++ {
+	for i := 0; i < 500000000; i ++ {
 
 		r := &resourcepb.Resource{
 			Labels: map[string]string{
@@ -78,9 +185,14 @@ func main(){
 
 		m := []*metricspb.Metric{
 			metricBuilder().setName("foo/metric").
-				setDataType(metricspb.MetricDescriptor_GAUGE_INT64).build(),
+				setDataType(metricspb.MetricDescriptor_GAUGE_INT64).
+				addTimeseries(time.Now().Unix(), []string{}).
+				addInt64Point(0, 0, time.Now().Unix()).build(),
+
 			metricBuilder().setName("bar/metric").
-				setDataType(metricspb.MetricDescriptor_GAUGE_INT64).build(),
+				setDataType(metricspb.MetricDescriptor_GAUGE_INT64).
+				addTimeseries(time.Now().Unix(), []string{}).
+				addInt64Point(0, 10, time.Now().Unix()).build(),
 		}
 
 		metrics, err := otlp.NewProtobufMetricsMarshaler().MarshalMetrics(opencensus.OCToMetrics(nil, r, m))
@@ -93,6 +205,6 @@ func main(){
 			panic(err)
 		}
 
-		time.Sleep(time.Second * 30)
+		time.Sleep(time.Second)
 	}
 }
